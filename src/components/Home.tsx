d61b8c2d476fc76a12682389562aa3265bd87112
Vladimir Borisov <v.borisov@uniteller.ru>
2023-07-28 19:49:22 +0300

    style: blocks changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -38,33 +38,12 @@ const Home = () => {
     }
   }, [word]);
 
-  if (items && !items[0].hwi) {
-    return (
-      <div className="container w-full mt-10 h-1/6 flex justify-between ">
-        <div className="bg-neutral-400 h-200 w-1/3 flex">
-          <div className="flex relative w-full mt-5 justify-center">
-            <input type="text"
-                   className="flex justify-center h-10 rounded w-3/4 p-2 pr-10"
-                   placeholder="Поиск"
-                   onChange={e => setWord(e.currentTarget.value)}/>
-          </div>
-        </div>
-
-        <div className=" w-3/4 ml-15 flex-grow flex flex-col h-max">
-          <div className="flex bg-blue-50 h-10 justify-between items-center ml-5 pl-5">
-            Поиск не дал результатов
-          </div>
-
-        </div>
-      </div>
-    );
-  }
-
     return (
       <div className="container w-full mt-10 h-1/6 flex justify-between ">
 
         <div className="bg-neutral-400 h-200 w-1/3 flex justify-center">
             <input type="text"
+                   value={word}
                    className="flex justify-center h-10 mt-5 rounded w-3/4 p-2 pr-10"
                    placeholder="Поиск"
                    onChange={e => setWord(e.currentTarget.value)}/>
@@ -73,6 +52,7 @@ const Home = () => {
 
         <div className=" relative w-2/4 ml-15 flex-grow flex flex-col h-max">
           {items && items.length > 0 && items.map((i, index) => {
+
             return <div className="flex bg-blue-50 h-10 mb-5 justify-between items-center ml-5 " key={index}>
               <div className="flex font-bold w-2/6 ml-2">{i.hwi?.hw ?? '-'}</div>
               <div className="flex italic w-2/6 mr-4">{i.fl ?? '-'}</div>
@@ -83,11 +63,12 @@ const Home = () => {
                          onClick={() => onclickHandler(i)}>&#9733;</div>
                   : <div className="flex ml-2 mr-4 cursor-pointer text-blue-400"
                          onClick={() => onclickHandlerRemove(i)}>&#9733;</div>
-                }
+                ?? null}
               </div>
             </div>;
           })
           }
+
         </div>
 
       </div>
--- src/components/StarredWords.tsx
+++ src/components/StarredWords.tsx
@@ -35,42 +35,45 @@ const StarredWords = () => {
 
   if (word && filtered) {
     return (
-      <div className="container">
-        <div className="input-container-starred">
+      <div className="container w-full mt-10 h-1/6 flex justify-between ">
+
+        <div className="bg-neutral-400 h-200 w-1/3 flex flex-col items-center">
+
           <input type="text"
-                 className="search-input"
+                 className="flex justify-center h-10 mt-5 rounded w-3/4 p-2 pr-10"
                  placeholder="Поиск"
                  onChange={e => setWord(e.currentTarget.value)}/>
 
-          <div className="checkboxBlock">
+          <div className="flex flex-col w-3/4 mt-2 ">
             <label>
-              <input type="checkbox" checked={isAdjectiveChecked} onChange={()=>dispatch(toggleAdjective())}/>
+              <input className="mr-2" type="checkbox" checked={isAdjectiveChecked}
+                     onChange={() => dispatch(toggleAdjective())}/>
               adjective
             </label>
             <label>
-              <input type="checkbox" checked={isNounChecked} onChange={()=>dispatch(toggleNoun())}/>
+              <input className="mr-2" type="checkbox" checked={isNounChecked} onChange={() => dispatch(toggleNoun())}/>
               noun
             </label>
             <label>
-              <input type="checkbox" checked={isVerbChecked} onChange={()=>toggleVerb}/>
+              <input className="mr-2" type="checkbox" checked={isVerbChecked} onChange={() => toggleVerb}/>
               verb
             </label>
           </div>
 
         </div>
 
-        <div className="items-container">
-
+        <div className=" relative w-2/4 ml-15 flex-grow flex flex-col h-max">
           {filtered && filtered.length > 0 && filtered.map((i, index) => {
-
-            return <div className="wordItem" key={index}>
-              <div className="wordInItem">{i.hwi?.hw ?? '-'}</div>
-              <div className="wordInItemType">{i.fl ?? '-'}</div>
-              <div className="wordMeaning">{i.shortdef ? i.shortdef[0] : '-'}</div>
-              <div className="starContainer">
+            return <div className="flex bg-blue-50 h-10 mb-5 justify-between items-center ml-5 " key={index}>
+              <div className="flex font-bold w-2/6 ml-2">{i.hwi?.hw ?? '-'}</div>
+              <div className="flex italic w-2/6 mr-4">{i.fl ?? '-'}</div>
+              <div className="flex w-3/4 truncate  mr-8">{i.shortdef ? i.shortdef[0] : '-'}</div>
+              <div className="flex w-1/10">
                 {!ids.includes(i.meta.id)
-                  ? <div className="starOutline" onClick={() => onclickHandler(i)}>&#9733;</div>
-                  : <div className="starFilled" onClick={() => onclickHandlerRemove(i)}>&#9733;</div>
+                  ? <div className="flex ml-2 mr-4 cursor-pointer text-gray-400"
+                         onClick={() => onclickHandler(i)}>&#9733;</div>
+                  : <div className="flex ml-2 mr-4 cursor-pointer text-blue-400"
+                         onClick={() => onclickHandlerRemove(i)}>&#9733;</div>
                 }
               </div>
             </div>;
@@ -81,50 +84,54 @@ const StarredWords = () => {
     );
   }
 
-
   return (
-    <div className="container">
-      <div className="input-container-starred">
+    <div className="container w-full mt-10 h-1/6 flex justify-between ">
+
+      <div className="bg-neutral-400 h-200 w-1/3 flex flex-col items-center">
+
         <input type="text"
-               className="search-input"
+               className="flex justify-center h-10 mt-5 rounded w-3/4 p-2 pr-10"
                placeholder="Поиск"
                onChange={e => setWord(e.currentTarget.value)}/>
 
-        <div className="checkboxBlock">
+        <div className="flex flex-col w-3/4 mt-2 ">
           <label>
-            <input type="checkbox" checked={isAdjectiveChecked} onChange={()=>dispatch(toggleAdjective())}/>
+            <input className="mr-2" type="checkbox" checked={isAdjectiveChecked}
+                   onChange={() => dispatch(toggleAdjective())}/>
             adjective
           </label>
           <label>
-            <input type="checkbox" checked={isNounChecked} onChange={()=>dispatch(toggleNoun())}/>
+            <input className="mr-2" type="checkbox" checked={isNounChecked} onChange={() => dispatch(toggleNoun())}/>
             noun
           </label>
           <label>
-            <input type="checkbox" checked={isVerbChecked} onChange={()=>toggleVerb}/>
+            <input className="mr-2" type="checkbox" checked={isVerbChecked} onChange={() => toggleVerb}/>
             verb
           </label>
         </div>
 
       </div>
 
-      <div className="items-container">
-
+      <div className=" relative w-2/4 ml-15 flex-grow flex flex-col h-max">
         {favorites && favorites.length > 0 && favorites.map((i, index) => {
-
-          return <div className="wordItem" key={index}>
-            <div className="wordInItem">{i.hwi?.hw ?? '-'}</div>
-            <div className="wordInItemType">{i.fl ?? '-'}</div>
-            <div className="wordMeaning">{i.shortdef ? i.shortdef[0] : '-'}</div>
-            <div className="starContainer">
+          return <div className="flex bg-blue-50 h-10 mb-5 justify-between items-center ml-5 " key={index}>
+            <div className="flex font-bold w-2/6 ml-2">{i.hwi?.hw ?? '-'}</div>
+            <div className="flex italic w-2/6 mr-4">{i.fl ?? '-'}</div>
+            <div className="flex w-3/4 truncate  mr-8">{i.shortdef ? i.shortdef[0] : '-'}</div>
+            <div className="flex w-1/10">
               {!ids.includes(i.meta.id)
-                ? <div className="starOutline" onClick={() => onclickHandler(i)}>&#9733;</div>
-                : <div className="starFilled" onClick={() => onclickHandlerRemove(i)}>&#9733;</div>
+                ? <div className="flex ml-2 mr-4 cursor-pointer text-gray-400"
+                       onClick={() => onclickHandler(i)}>&#9733;</div>
+                : <div className="flex ml-2 mr-4 cursor-pointer text-blue-400"
+                       onClick={() => onclickHandlerRemove(i)}>&#9733;</div>
               }
             </div>
           </div>;
         })
         }
+
       </div>
+
     </div>
   );
 };
